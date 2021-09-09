473519a3e5c51d077e93677d68cc54faf4f57e69
Iraicy <iraicyahh@gmail.com>
2021-09-09 01:49:59 -0500

    validación formulario, petición api

--- src/componentes/Formulario.jsx
+++ src/componentes/Formulario.jsx
@@ -1,14 +1,54 @@
-import React from "react";
+
+import React ,{useState} from "react";
 
 function Formulario() {
+  const [formState, setFormState] = useState({
+    name:"",
+    country:"",
+  });
+
+  const [UsePredictionstate, setUsePredictionstate] = useState({})
+
+  const {name, country} = formState;
+
+  const handleChange = (e) =>{
+    setFormState({
+      ...formState,
+      [e.target.name] : e.target.value
+    }
+    )
+  };
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+
+    if(!name.trim()|| !country.trim()){
+      console.log("campos vacios");
+      return;
+    }
+    getPrediction();
+  };
+
+  const getPrediction = async() => {
+    const response = await fetch(`https://api.agify.io?name=${name}&country_id=${country}`)
+    const data = await response.json()
+    setUsePredictionstate(data)
+  };
   return (
     <div className="container">
       <h1>Formulario</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label className="mt-3">Escribe tu nombre</label>
-        <input className="form-control" type="text"></input>
+        <input className="form-control"
+          type="text"
+          name="name"
+          onChange={handleChange}
+        >
+        </input>
         <label className="mt-2">Escoge tu país</label>
-        <select className="form-control">
+        <select className="form-control"
+          name="country"
+          onChange={handleChange}
+        >
           <option value="">--Selecciona tu país--</option>
           <option value="US">Estados Unidos</option>
           <option value="VE">Venezuela</option>
